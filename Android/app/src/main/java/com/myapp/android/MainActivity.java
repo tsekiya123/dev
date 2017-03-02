af5f5345250e7cbb11c1c4be6f3666b6f3e3e222
関屋達貴 <t_sekiya@sekiyatooruki-no-MacBook-Pro.local>
2017-03-03 04:00:58 +0900

    通知画面

--- Android/app/src/main/java/com/myapp/android/MainActivity.java
+++ Android/app/src/main/java/com/myapp/android/MainActivity.java
@@ -3,8 +3,12 @@ package com.myapp.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
 
         String test = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "test is: " + test);
+
+        TextView dateText = (TextView)findViewById(R.id.textView2);
+        // 現在の時刻を取得
+        Date date = new Date();
+        // 表示形式を設定
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy'年'MM'月'dd'日'　kk'時'mm'分'ss'秒'");
+        dateText.setText(sdf.format(date));
     }
 }
--- Android/app/src/main/java/com/myapp/android/fcm/FcmService.java
+++ Android/app/src/main/java/com/myapp/android/fcm/FcmService.java
@@ -1,9 +1,20 @@
 package com.myapp.android.fcm;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.myapp.android.MainActivity;
+import com.myapp.android.R;
 
 
 /**
@@ -18,9 +29,77 @@ public class FcmService extends FirebaseInstanceIdService{
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
 
+        sendNotification();
+
         // If you want to send messages to this application java.lang.Stringinstance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
         // sendRegistrationToServer(refreshedToken);
+    }
+
+    private static final int NOTIFICATION_ID = 0;
+    private static final int REQUEST_CODE = 0;
+    private static final long[] vibration = {0, 1000, 500, 1000, 500, 1000};
+    private static final int requestCode = 0;
+
+    private void sendNotification() {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Title"); // 1行目
+        builder.setSound(Settings.System.DEFAULT_ALARM_ALERT_URI);
+        builder.setVibrate(vibration);
+
+        // 適当なIntent
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(
+                getApplicationContext(),
+                REQUEST_CODE,
+                intent,
+                PendingIntent.FLAG_CANCEL_CURRENT
+        );
+        builder.setContentIntent(contentIntent);
+        // http://tiro105.hateblo.jp/entry/2014/05/28/152909
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+        manager.notify(NOTIFICATION_ID, builder.build());
+
+        // 通知バー押下時にアプリ再立ち上げ
+        // アプリ起動中は通知バーを表示せず、画面更新
+        // Intentの設定
+
+        // 気になる事
+        // getSystemService()
+        // MainApplication.getContext
+
+
+
+//        Notification noti = new Notification.Builder(getApplicationContext())
+//                .setContentTitle("Content Title")
+//                .setContentText("Content Text")
+//                .setSmallIcon(R.drawable.new_mail)
+//                .setLargeIcon(aBitmap)
+//                .build();
+//        Notification n = new Notification(); // Notificationの生成
+//        n.icon = R.drawable.coda; // アイコンの設定
+//        n.tickerText = "This is a notification message..."; // メッセージの設定
+//
+//        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+//        PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+//        n.setLatestEventInfo(getApplicationContext(), "TITLE", "TEXT", pi);
+//        n.contentIntent.
+//
+//        long[] vibrate_ptn = {0, 100, 300, 1000}; // 独自バイブレーションパターン
+//        n.vibrate = vibrate_ptn; // 独自バイブレーションパターンを設定
+//
+//        n.defaults |= Notification.DEFAULT_LIGHTS; // デフォルトLED点滅パターンを設定
+//
+//        // NotificationManagerのインスタンス取得
+//        NotificationManager nm =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        nm.notify(1, n); // 設定したNotificationを通知する
     }
 }
